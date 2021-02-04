2b5c08414b14d76914761235042dd96ddc73c43c
davidkuyk <davidkuyk@gmail.com>
2021-02-04 14:13:43 -0600

    Update China source.

--- thedailyworld.py
+++ thedailyworld.py
@@ -8,45 +8,45 @@ driver = webdriver.Chrome()
 path = os.path.dirname(os.path.realpath(__file__)) + '/thedailyworld.html'
 
 entities = {
-    'United Nations': {'abbr': 'UN','source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'World Trade Organization': {'abbr': 'WTO', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '}, 
-    'International Monetary Fund': {'abbr': 'IMF', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'World Bank': {'abbr': 'World Bank', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'European Union': {'abbr': 'EU', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'World Health Organization': {'abbr': 'WHO', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'National Endowment for Democracy': {'abbr': 'NED', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'USAID': {'abbr': 'USAID', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'NATO': {'abbr': 'NATO', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'China': {'abbr': 'China', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'India': {'abbr': 'India', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'United States': {'abbr': 'U.S.', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Indonesia': {'abbr': 'Indonesia', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Pakistan': {'abbr': 'Pakistan', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Brazil': {'abbr': 'Brazil', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Nigeria': {'abbr': 'Nigeria', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Bangladesh': {'abbr': 'Bangladesh', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Russia': {'abbr': 'Russia', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Mexico': {'abbr': 'Mexico', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Japan': {'abbr': 'Japan', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Ethiopia': {'abbr': 'Ethiopia', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Philippines': {'abbr': 'Philippines', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Egypt': {'abbr': 'Egypt', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Vietnam': {'abbr': 'Vietnam', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Democratic Republic of Congo': {'abbr': 'DR Congo', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Germany': {'abbr': 'Germany', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Turkey': {'abbr': 'Turkey', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Iran': {'abbr': 'Iran', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Thailand': {'abbr': 'Thailand', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'United Kingdom': {'abbr': 'UK', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'France': {'abbr': 'France', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Italy': {'abbr': 'Italy', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'South Africa': {'abbr': 'South Africa', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Tanzania': {'abbr': 'Tanzania', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Myanmar': {'abbr': 'Myanmar', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Kenya': {'abbr': 'Kenya', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'South Korea': {'abbr': 'South Korea', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Colombia': {'abbr': 'Colombia', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
-    'Spain': {'abbr': 'Spain', 'source': 'https://news.google.com/search?q=', 'location': 'article > h3 > a '},
+    'United Nations': {'abbr': 'UN','parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=united%20nations', 'location': 'article > h3 > a '},
+    'World Trade Organization': {'abbr': 'WTO', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=world%20trade%20organization', 'location': 'article > h3 > a '}, 
+    'International Monetary Fund': {'abbr': 'IMF', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=International%20Monetary%20Fund', 'location': 'article > h3 > a '},
+    'World Bank': {'abbr': 'World Bank', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=World%20Bank', 'location': 'article > h3 > a '},
+    'European Union': {'abbr': 'EU', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=European%20Union', 'location': 'article > h3 > a '},
+    'World Health Organization': {'abbr': 'WHO', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=World%20Health%20Organization', 'location': 'article > h3 > a '},
+    'National Endowment for Democracy': {'abbr': 'NED', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=National%20Endowment%20for%20Democracy', 'location': 'article > h3 > a '},
+    'USAID': {'abbr': 'USAID', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=USAID', 'location': 'article > h3 > a '},
+    'NATO': {'abbr': 'NATO', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=NATO', 'location': 'article > h3 > a '},
+    'China': {'abbr': 'China', 'parent': 'https://www.scmp.com', 'source': 'https://www.scmp.com/news/china', 'location': '.article-title__article-link'},
+    'India': {'abbr': 'India', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=India', 'location': 'article > h3 > a '},
+    'United States': {'abbr': 'U.S.', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=U.S.', 'location': 'article > h3 > a '},
+    'Indonesia': {'abbr': 'Indonesia', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Indonesia', 'location': 'article > h3 > a '},
+    'Pakistan': {'abbr': 'Pakistan', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Pakistan', 'location': 'article > h3 > a '},
+    'Brazil': {'abbr': 'Brazil', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Brazil', 'location': 'article > h3 > a '},
+    'Nigeria': {'abbr': 'Nigeria', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Nigeria', 'location': 'article > h3 > a '},
+    'Bangladesh': {'abbr': 'Bangladesh', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Bangladesh', 'location': 'article > h3 > a '},
+    'Russia': {'abbr': 'Russia', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Russia', 'location': 'article > h3 > a '},
+    'Mexico': {'abbr': 'Mexico', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Mexico', 'location': 'article > h3 > a '},
+    'Japan': {'abbr': 'Japan', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Japan', 'location': 'article > h3 > a '},
+    'Ethiopia': {'abbr': 'Ethiopia', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Ethiopia', 'location': 'article > h3 > a '},
+    'Philippines': {'abbr': 'Philippines', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Philippines', 'location': 'article > h3 > a '},
+    'Egypt': {'abbr': 'Egypt', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Egypt', 'location': 'article > h3 > a '},
+    'Vietnam': {'abbr': 'Vietnam', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Vietnam', 'location': 'article > h3 > a '},
+    'Democratic Republic of Congo': {'abbr': 'DR Congo', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=DR%20Congo', 'location': 'article > h3 > a '},
+    'Germany': {'abbr': 'Germany', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Germany', 'location': 'article > h3 > a '},
+    'Turkey': {'abbr': 'Turkey', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Turkey', 'location': 'article > h3 > a '},
+    'Iran': {'abbr': 'Iran', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Iran', 'location': 'article > h3 > a '},
+    'Thailand': {'abbr': 'Thailand', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Thailand', 'location': 'article > h3 > a '},
+    'United Kingdom': {'abbr': 'UK', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=U.K.', 'location': 'article > h3 > a '},
+    'France': {'abbr': 'France', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=France', 'location': 'article > h3 > a '},
+    'Italy': {'abbr': 'Italy', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Italy', 'location': 'article > h3 > a '},
+    'South Africa': {'abbr': 'South Africa', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=South%20Africa', 'location': 'article > h3 > a '},
+    'Tanzania': {'abbr': 'Tanzania', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Tanzania', 'location': 'article > h3 > a '},
+    'Myanmar': {'abbr': 'Myanmar', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Myanmar', 'location': 'article > h3 > a '},
+    'Kenya': {'abbr': 'Kenya', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Kenya', 'location': 'article > h3 > a '},
+    'South Korea': {'abbr': 'South Korea', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=South%20Korea', 'location': 'article > h3 > a '},
+    'Colombia': {'abbr': 'Colombia', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Colombia', 'location': 'article > h3 > a '},
+    'Spain': {'abbr': 'Spain', 'parent': 'https://news.google.com', 'source': 'https://news.google.com/search?q=Spain', 'location': 'article > h3 > a '},
 }
 
 playFile = open(path, 'w')
@@ -104,56 +104,56 @@ playFile.flush()
 print('Gathering headlines...')
 
 # loop through entities
-try:
-    i = 0
-    for key, values in entities.items():
-        time.sleep(randint(0, 2))
-        if i == 3:
-            driver.get('file://' + path)
-        searchTerm = values['source'] + key
-        res = requests.get(searchTerm)
-        res.raise_for_status()
-        noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
-        headline = noStarchSoup.select(values['location'])
-        title = headline[0].text
-        link = 'https://news.google.com' + (headline[0].attrs['href']).lstrip('.')
+# try:
+i = 0
+for key, values in entities.items():
+    time.sleep(randint(0, 2))
+    if i == 3:
+        driver.get('file://' + path)
+    searchTerm = values['source']
+    res = requests.get(searchTerm)
+    res.raise_for_status()
+    noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
+    headline = noStarchSoup.select(values['location'])
+    title = headline[0].text
+    link = values['parent'] + (headline[0].attrs['href']).lstrip('.')
 
-        # filter articles with "opinion" in the title
-        if "opinion" in title or "Opinion" in title:
-            title = headline[1].text
-            link = 'https://news.google.com' + (headline[1].attrs['href']).lstrip('.')
+    # filter articles with "opinion" in the title
+    if "opinion" in title or "Opinion" in title:
+        title = headline[1].text
+        link = values['parent'] + (headline[1].attrs['href']).lstrip('.')
 
-        # if title is missing, manually get article title from link
-        if len(title) == 0:
-            try:
-                res = requests.get(link)
-                res.raise_for_status()
-                noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
-                headline = noStarchSoup.select('body h1')
-                title = headline[0].text.strip()
-            except:
-                title = "No Title"
-        entityName = "<span class='entityName'>" + values['abbr'] + "</span>: "
-        entityLink = values['source'] + key.replace(' ', '%20')
-        # if no link
-        if len(link) == 0:
-            link = 'No Link'
-        # add divider below organizations
-        if i < 8:
-            playFile.write(("<h1><a href={4} target='_blank'><div class='tooltip'><span class='tooltiptext'>{3}</span>{0}</a></div> <a href={2} target='_blank'>{1}</a></h1>").format(entityName, title, link, countries[i], entityLink))
-        elif i == 8:
-            playFile.write(("<h1><a href={4} target='_blank'><div class='tooltip'><span class='tooltiptext'>{3}</span>{0}</a></div> <a href={2} target='_blank'>{1}</a></h1><hr>").format(entityName, title, link, countries[i], entityLink))
-        else:
-            playFile.write(("<h1><a href={3} target='_blank'>{0}</a><a href={2} target='_blank'>{1}</a></h1>").format(entityName, title, link, entityLink))
-        # clear and print progress
-        os.system('clear')
-        if i == 0 or i % 3 == 0:
-            playFile.flush()
-            driver.refresh()
-        print(f"{i+1}/{len(countries)}")
-        i += 1
-except Exception as err:
-    print('An exception happened: ' + str(err))
+    # if title is missing, manually get article title from link
+    if len(title) == 0:
+        try:
+            res = requests.get(link)
+            res.raise_for_status()
+            noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
+            headline = noStarchSoup.select('body h1')
+            title = headline[0].text.strip()
+        except:
+            title = "No Title"
+    entityName = "<span class='entityName'>" + values['abbr'] + "</span>: "
+    entityLink = values['source']
+    # if no link
+    if len(link) == 0:
+        link = 'No Link'
+    # add divider below organizations
+    if i < 8:
+        playFile.write(("<h1><a href={4} target='_blank'><div class='tooltip'><span class='tooltiptext'>{3}</span>{0}</a></div> <a href={2} target='_blank'>{1}</a></h1>").format(entityName, title, link, key, entityLink))
+    elif i == 8:
+        playFile.write(("<h1><a href={4} target='_blank'><div class='tooltip'><span class='tooltiptext'>{3}</span>{0}</a></div> <a href={2} target='_blank'>{1}</a></h1><hr>").format(entityName, title, link, key, entityLink))
+    else:
+        playFile.write(("<h1><a href={3} target='_blank'>{0}</a><a href={2} target='_blank'>{1}</a></h1>").format(entityName, title, link, entityLink))
+    # clear and print progress
+    os.system('clear')
+    if i == 0 or i % 3 == 0:
+        playFile.flush()
+        driver.refresh()
+    print(f"{i+1}/{len(entities)}")
+    i += 1
+# except Exception as err:
+    # print('An exception happened: ' + str(err))
 playFile.close()
-os.system('clear')
+# os.system('clear')
 print('Done.')
